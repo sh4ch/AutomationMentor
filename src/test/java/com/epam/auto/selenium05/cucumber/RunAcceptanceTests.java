0721b19a09fd92c4eba0a80b13c28115ad41ac7f
Yulia Devyatkina <sh4jch@gmail.com>
2023-09-11 09:33:09 +0200

    [homework-Selenium05] Implement BDD framework

---
+++ src/test/java/com/epam/auto/selenium05/cucumber/RunAcceptanceTests.java
@@ -0,0 +1,8 @@
+package com.epam.auto.selenium05.cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(plugin = {"pretty"})
+public class RunAcceptanceTests extends AbstractTestNGCucumberTests {
+}
