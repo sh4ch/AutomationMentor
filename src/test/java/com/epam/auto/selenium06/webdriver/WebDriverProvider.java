07dba2b5be904ae2e54afe3356f11f0f43c441b6
Yulia Devyatkina <sh4jch@gmail.com>
2023-09-14 23:41:24 +0200

    [homework-Selenium06] Implement Selenium-grid task

---
+++ src/test/java/com/epam/auto/selenium06/webdriver/WebDriverProvider.java
@@ -0,0 +1,28 @@
+package com.epam.auto.selenium06.webdriver;
+
+import java.util.Locale;
+import java.util.Objects;
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverProvider {
+    private static WebDriver driver;
+
+    private WebDriverProvider() {
+    }
+
+    public static WebDriver getDriver() {
+        if (Objects.isNull(driver)) {
+            String launchType = System.getProperty("launch.type", "local");
+            String browserName = System.getProperty("browser.name", "chrome");
+            driver = WebDriverFactory.createWebDriver(launchType,
+                    Browser.valueOf(browserName.toUpperCase(Locale.ROOT)));
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+        driver = null;
+    }
+
+}
